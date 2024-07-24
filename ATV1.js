e34a8ffdc173a60c6e69bdfba61648619953f43a
IagorBM <iagorbremmmalaguti@gmail.com>
2024-07-24 16:10:09 -0300

    atv1

---
+++ ATV1.js
@@ -0,0 +1,9 @@
+document.getElementById("fato").addEventListener("click", fato);
+function fato(){
+    console.log("aasdqweqw");
+    let multip = 1;
+    var fat1 = document.getElementById("fat1").value;
+    
+    multip = parseInt(multip);
+    document.getElementById("respos1taFat").innerHTML= fat1;
+}
